49079f58780292c9b9fa3f2aaabdf26f4427bea3
yairmau <yairmau@gmail.com>
2024-05-08 16:48:23 +0300

    blink external led

--- docs/archive/code/blink_external_LED.ino
+++ docs/archive/code/blink_external_LED.ino
@@ -1,13 +1,15 @@
+const int LED_pin = A4;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_pin, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);                       // wait for 500 milliseconds
+  digitalWrite(LED_pin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
